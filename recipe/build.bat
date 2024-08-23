3781e3456db2b0712c0fd53a8a21b01977c7eae0
Alfredo Luque <alfredo.luque@airbnb.com>
2024-08-23 22:19:16 +0000

    update for windows

--- recipe/build.bat
+++
@@ -1,4 +0,0 @@
-REM Build the Go project
-REM Note: We use go build instead of go install due to cross-compilation issues
-REM Reference: https://github.com/golang/go/issues/57485
-go build -v -o "%LIBRARY_BIN%\s5cmd.exe"
